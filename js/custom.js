b02b61970b6c0493ab3b29cad3cfed981bb5f308
ujjavals <ujjavals@fomaxtech.com>
2014-05-02 16:22:16 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
-	
-	
-	
-	
-	
-});
+var valid_extensions = /(.pdf|.docx|.doc)$/i;
+var num =/^\d+$/;
+var feed_city_pattern = /^[A-Za-z ]{3,80}$/;
+var feed_city_patternd = /^[A-Za-z. ]{3,80}$/;
+var mobile_pattern =/^\d{4,10}$/;
+var regdate = /^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d+$/;
+var url =/^([w*3]+(\.[a-zA-Z0-9]+)+.*)$/;
+var mobileline  = /^(\+)?([0-9]{1,4})?\d{8,12}$/;
+var landline  = /^(\+)?([0-9]{1,4})?([ ]{1,1})?(\-{0,1})?\d{8,12}$/;
 
 var doctor_value ="";
 var qualification_value="";
@@ -134,46 +136,168 @@ function appointmentsubmit(){
 		var user_email =   $("#user_email").val();
 		var user_mobilenumber =   $("#user_mobilenumber").val();
 		var user_comment =   $("#user_comment").val();
-		$(".page_loading").css("display","block");
+		var error = 0;
+		var nam = document.getElementById('user_name'); 
+		var emailId = document.getElementById('user_email'); 
+		var phone = document.getElementById('user_mobilenumber');
 		
-		$.ajax({
-			url :	"http://fomax.us/Bangalore-doctor/Appointment2.php" ,
-			type : "POST",
-			dataType : "html",
-			data: {
-			"user": user_name,
-			"emailid": user_email,
-			"mobile": user_mobilenumber,
-			"doctor_value": doctor_value,
-			"qualification_value": qualification_value,
-			"location_value": location_value,
-			"purpose" : user_comment,
-			},
-			success : function(res)
+		if ((nam.value == "Enter name")||(nam.value == "")){
+			alert("Please Enter Your Name");
+			nam.focus();
+			error =1;
+			return;
+		}
+		
+		//Email id
+	
+		   if((emailId.value == "Enter email") || (emailId.value == "")){
+				alert("Enter Your Email Address");
+				emailId.focus();
+				error =1;
+				return;
+			}else{
+			if(!isValidEmail(emailId.value))
 			{
-				
-				if(res==1){
-					$(".page_loading").css("display","none");
-					alert("Appointment is completed successfully.");
-					//$("#user_name").reset();
-					 document.getElementById("user_name").value="";
-					 document.getElementById("user_email").value="";
-					 document.getElementById("user_mobilenumber").value="";
-					 document.getElementById("user_comment").value="";
-				
-					 location.reload(); 
-				}
-				
-				
-				
-			
-			},
-			error: function(rtn){
-				//	alert("error");
+				alert("Enter Valid Email Address");
+				emailId.focus();
+				error =1;
+				return;
+			}
 			}
 			
-		});
+		//Phone Number
+		if (!ValidPhone(phone.value))
+		{
+			phone.focus();
+			error =1;
+			return;
+		}	
+		
+		
+		if(error ==0){
+		
+				
+				
+				
+				$(".page_loading").css("display","block");
+				
+				$.ajax({
+					url :	"http://fomax.us/Bangalore-doctor/Appointment2.php" ,
+					type : "POST",
+					dataType : "html",
+					data: {
+					"user": user_name,
+					"emailid": user_email,
+					"mobile": user_mobilenumber,
+					"doctor_value": doctor_value,
+					"qualification_value": qualification_value,
+					"location_value": location_value,
+					"purpose" : user_comment,
+					},
+					success : function(res)
+					{
+						
+						if(res==1){
+							$(".page_loading").css("display","none");
+							alert("Appointment is completed successfully.");
+							//$("#user_name").reset();
+							 document.getElementById("user_name").value="";
+							 document.getElementById("user_email").value="";
+							 document.getElementById("user_mobilenumber").value="";
+							 document.getElementById("user_comment").value="";
+						
+							 location.reload(); 
+						}
+						
+						
+						
+					
+					},
+					error: function(rtn){
+						//	alert("error");
+					}
+					
+				});
+		
+		}
 		
 
+}
+
+
+
+function isValidEmail(e)
+{
+    
+    var exp=/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+    result = exp.test(e);
+    if (result)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function ValidPhone(phone)
+{
+    var reg_mobile= /^(\+)?([0-9]{8,15})?\d{8,15}$/;
+    var valid = "0123456789";
+    if((phone=="") || (phone<=0))
+    {
+        alert ("Please Enter Mobile Number")
+        return false
+    }    
+    else if(!reg_mobile.test(phone))
+    {
+        alert("Invalid Mobile Number Length! Please Try Again.")
+        return false
+    }
+        
+    return true
+}
+
+//Number Validation
+function valid1(f) {
+   // !(/^[ÃƒÂ±0-9( )+\/#-]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÂ±0-9( )+#\/-]/ig,''):null;
+    !(/^[ÃƒÂ0-9]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÂ0-9]/ig,''):null;
+}
+
+function valid88(f) {
+   // !(/^[ÃƒÂ±0-9( )+\/#-]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÂ±0-9( )+#\/-]/ig,''):null;
+    !(/^[ÃƒÂ0-9+-]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÂ0-9]/ig,''):null;
+}
+function valid77(f) {
+   // !(/^[ÃƒÂ±0-9( )+\/#-]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÂ±0-9( )+#\/-]/ig,''):null;
+    !(/^[ÃƒÂ0-9+]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÂ0-9]/ig,''):null;
+}
+
+function valid67(f) {
+    !(/^[ÃƒÂ0-9+.]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÂ0-9]/ig,''):null;
+}
+function validc(f) {
+	   !(/^[ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z( )+ÃƒÂ0-9+.]*$/i ).test(f.value)?f.value = f.value.replace(/[^ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z( )+ÃƒÂ0-9+.]/ig,''):null;
+   // !(/^[ÃƒÂ0-9+.]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÂ0-9]/ig,''):null;
+}
+function valid2(f) {
+    !(/^[ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z( )+\/#-]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z( )+#\/-]/ig,''):null;
+}
+function valid3(f) {
+    !(/^[ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Â ÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢ÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬Ã‚Â ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬ÃƒÂ¢Ã¢â‚¬Å¾Ã‚Â¢ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬Ãƒâ€¦Ã‚Â¡ÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬Ã…Â¡ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z( )+\/-]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Â ÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢ÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬Ã‚Â ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬ÃƒÂ¢Ã¢â‚¬Å¾Ã‚Â¢ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬Ãƒâ€¦Ã‚Â¡ÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬Ã…Â¡ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z( )+\/-]/ig,''):null;
+}
+/*Name Validation*/
+function valid4(f) {
+    !(/^[ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z]*$/i ).test(f.value)?f.value = f.value.replace(/[^ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z]/ig,''):null;
+}
+/*website*/
+function valid9(f) {
+    !(/^[ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z+.+ÃƒÂ0-9]*$/i ).test(f.value)?f.value = f.value.replace(/[^ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z( )+.+ÃƒÂ0-9]/ig,''):null;
+}
+
+/*company validation*/
+function valid5(f) {
+    !(/^[ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z. ]*$/i).test(f.value)?f.value = f.value.replace(/[^ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â±a-z]/ig,''):null;
 }
 
